cc6ad601e46853ee550e940b2ce2a4dbfd06da9d
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-06-05 21:01:42 -0500

    Inclusión de routing al proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { CharacterPageComponent } from './characters/pages/character-page/character-page.component';
+import { HomePageComponent } from './characters/pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'character',
+    component: CharacterPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/characters/pages/character-page/character-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'character-character-page',
+    templateUrl: './character-page.component.html',
+    styleUrls: ['./character-page.component.css'],
+})
+export class CharacterPageComponent { }
